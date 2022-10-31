b6d0e8c2ecdb1a441ffec15bbbb867d927c15521
Favour Okechukwu <favourapps17@gmail.com>
2022-10-31 16:09:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Request-Method: GET");
 
-$arr["slackusername"] = "Favour Okechukwu";
+$arr["slackUsername"] = "Favour Okechukwu";
 $arr["backend"]= true;
 $arr["age"] =23;
 $arr["bio"]= "am a php programmer,i build web applications using php and its frameworks,am a software Engineer ";
