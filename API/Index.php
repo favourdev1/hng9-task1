ac79309a1cf39562d1c45a03454438f7c8b32ac8
Favour Okechukwu <favourapps17@gmail.com>
2022-10-31 15:11:43 +0100

    Create Index.php

---
+++ API/Index.php
@@ -0,0 +1,8 @@
+<?php
+
+$arr["slackusername"] = "Favour Okechukwu";
+$arr["backend"]= true;
+$arr["age"] =23;
+$arr["bio"]= "am a php programmer,i build web applications using php and its frameworks,am a software Engineer ";
+
+echo json_encode($arr);
